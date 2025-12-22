1811a7abd1e87232c3e19e531107b2554b401abc
mhsakh01@gmail.com <mhsakh01@gmail.com>
2025-12-22 18:48:34 +0330

    indexfilecode

--- wekad/wwwroot/js/site.js
+++ wekad/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        document.querySelector("nav .active")?.classList.remove("active");
+        link.classList.add("active");
+    });
+});
