0977bfe5676146178746280ae4e8596859584201
zahrafalhi01 <zahrafalhi01@gmail.com>
2025-12-31 19:47:05 +0000

    remove

--- wekad/wwwroot/js/site.js
+++ wekad/wwwroot/js/site.js
@@ -1,11 +1,1 @@
-﻿const navItems = document.querySelectorAll(".nav-item");
-
-navItems.forEach(item => {
-    item.addEventListener("mouseover", () => {
-        item.classList.add("nav-hover");
-    });
-
-    item.addEventListener("mouseout", () => {
-        item.classList.remove("nav-hover");
-    });
-});
+﻿
