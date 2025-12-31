c2f08a953756851cb167468af9262b4c00224561
zahrafalhi01 <zahrafalhi01@gmail.com>
2025-12-31 19:49:38 +0000

    add4

--- wekad/wwwroot/js/site.js
+++ wekad/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿
+﻿const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.classList.add("nav-hover");
+    });
+
+    item.addEventListener("mouseout", () => {
+        item.classList.remove("nav-hover");
+    });
+});
